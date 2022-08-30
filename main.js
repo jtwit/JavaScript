29afc12f09dac9f9160d2c781150d7ddfe212f62
jt.wit <jt.wit2022@startsteps.de>
2022-08-30 14:46:10 +0200

    change comment test

--- main.js
+++ main.js
@@ -171,7 +171,7 @@ newfuction(array, index)
 
 // dupFunction(array)
 
-// exercise 11 final
+// exercise 11 final final
 
 var numbers = [3, 6, 67, 6, 23, 11, 100, 8, 93, 0, 17, 24, 7, 1, 33, 45, 28, 33, 23, 12, 99, 100]
 console.log(numbers.length)
